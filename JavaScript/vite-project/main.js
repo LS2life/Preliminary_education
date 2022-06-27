72715860bf5345f1936889387642a69f9b3bd392
E_San <ls2wist@gmail.com>
2022-06-27 16:37:56 +0900

    js up

---
+++ JavaScript/vite-project/main.js
@@ -0,0 +1,39 @@
+import './style.css'
+
+console.log("Hello World by main.js")
+//alert("Hello World by main.js")  경고창
+
+for(let i=0; i<3; i++) {
+    console.log(i)
+}
+let sum=10+20; console.log(sum);
+let sum2=10+20
+console.log(sum2)
+let sum3= 
+    10+40;
+
+let pi=3.14
+let pi1=3.14
+
+const p3="상수"
+//문자열 자료형
+str1="안녕하세요"
+str2='안녕하세요'
+str3="'안녕'하세요"
+
+let c1=10
+let c2="10"
+console.log(c1==c2)
+console.log(c1===c2)
+console.loa(!true)
+let b1=true
+let b2=felse
+console.log( b1?"b1 is true":"b1 is false")
+console.log( b1 && b2?"true":"false")
+console.log( b1 || b2?"true":"false")
+
+let any
+any=1
+any="문자"
+console.log("type of any" + typeof(any))
+
