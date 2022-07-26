b620fa57a7ae0725204450b68357505fbe54bcfc
ESan <ls2wist@gmail.com>
2022-07-27 07:16:47 +0900

    update

---
+++ spring5/prectice/src/main/java/com/example/demo/PrecticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrecticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrecticeApplication.class, args);
+	}
+
+}
