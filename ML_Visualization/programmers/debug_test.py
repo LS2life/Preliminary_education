e4b16c63436982c32d50779d1b8ece96de22897e
lss <ls2wist@gmail.com>
2022-10-27 01:58:23 +0900

    programmers update

--- ML_Visualization/programmers/debug_test.py
+++ ML_Visualization/programmers/debug_test.py
@@ -1,7 +1,24 @@
-def solution(i):
-	answer=0
-	for i in range(0,i+1):
+def solution(num_list):
+	answer = []
+	for i in num_list:
 		if i%2==1:
-			continue
-		answer += i
+			a=i
+		elif i%2 != 1:
+			b=i
+	answer.extend([a,b])
 	return answer
+
+num_list=[1,2,3,4,5,]
+n2=[1,3,5,7]
+
+solution(num_list)
+
+
+# def solution(i):
+# 	answer=0
+# 	for i in range(0,i+1):
+# 		if i%2==1:
+# 			continue
+# 		answer += i
+# 	return answer
+
