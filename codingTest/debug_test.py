39b8e1047507bd23ac1248eff2e71483c0597d3c
lss <ls2wist@gmail.com>
2022-10-28 12:31:18 +0900

    good morning

---
+++ codingTest/debug_test.py
@@ -0,0 +1,34 @@
+a=[1,2,100,-99,1,2,3,]
+
+def sol(a):
+	answer=[]
+	for i in a:
+		return i
+		
+
+sol(a)
+
+# def solution(num_list):
+# 	answer = []
+# 	for i in num_list:
+# 		if i%2==1:
+# 			a=i
+# 		elif i%2 != 1:
+# 			b=i
+# 	answer.extend([a,b])
+# 	return answer
+
+# num_list=[1,2,3,4,5,]
+# n2=[1,3,5,7]
+
+# solution(num_list)
+
+
+# def solution(i):
+# 	answer=0
+# 	for i in range(0,i+1):
+# 		if i%2==1:
+# 			continue
+# 		answer += i
+# 	return answer
+
