d97a920d2d632a0108690641a752ed0744585a7d
lss <ls2wist@gmail.com>
2022-11-03 18:36:48 +0900

    spring update

---
+++ Svelte+SpringBoot/playground_spring_boot/kellogg/src/main/java/io/github/kellogg/KelloggApplication.java
@@ -0,0 +1,13 @@
+package io.github.kellogg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KelloggApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KelloggApplication.class, args);
+	}
+
+}
---
+++ Svelte+SpringBoot/playground_spring_boot/kellogg/src/test/java/io/github/kellogg/KelloggApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kellogg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KelloggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
