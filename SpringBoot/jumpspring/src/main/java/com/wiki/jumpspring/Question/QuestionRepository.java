1ca6746c9d9c4dc696199a493b85fa525bee4fdd
lss <ls2wist@gmail.com>
2022-12-15 20:34:27 +0900

    spring boot maven

--- SpringBoot/jumpspring/src/main/java/com/wiki/jumpspring/Question/QuestionRepository.java
+++ SpringBoot/jumpspring/src/main/java/com/wiki/jumpspring/Question/QuestionRepository.java
@@ -6,8 +6,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+// import org.springframework.data.jpa.repository.Query;
+// import org.springframework.data.repository.query.Param;
 
 public interface QuestionRepository extends JpaRepository<Question,Integer> {
     Question findBySubject(String subject);
---
+++ project/springboot/page/src/main/java/com/home/page/ttk/Board.java
@@ -0,0 +1,15 @@
+package com.home.page.ttk;
+
+import java.time.LocalDateTime;
+
+import lombok.ToString;
+
+@ToString
+public class Board {
+
+    private int boardNo;
+    private String title;
+    private String content;
+    private String writer;
+    private LocalDateTime regDate;
+}
