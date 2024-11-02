8e00a0349337011628d1c1d1ee121a35898a76d8
E_San <ls2wist@gmail.com>
2024-11-02 21:49:39 +0900

    ddfs

---
+++ python/python_start/pygame_basic/1_create_frame.py
@@ -0,0 +1,16 @@
+
+import pygame
+
+# 초기화 
+pygame.init()
+
+# 화면크기 설정
+# 가로크기
+scren_width = 480
+# 세로크기
+screen_height = 640
+screen = pygame.display.set_mode((screen_height, screen_width))
+
+# 화면 타이틀 설정
+# 게임 이름
+pygame.display.set_caption("SS Game")
